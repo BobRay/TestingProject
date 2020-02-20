327cc4cf36564a401aff0cbb792a57e8e72b5fdd
bobray <bobray@softville.com>
2020-02-20 16:50:02 -0500

    Add T2_UserErrorTest and user2.class.php

---
+++ _build/tests/unit/T2_UserErrorTest.php
@@ -0,0 +1,127 @@
+<?php
+use \Codeception\Stub;
+
+class userErrorTest extends \Codeception\Test\Unit {
+
+    /**
+     * @var $tester \UnitTester
+     */
+    protected $tester;
+    protected $validUsername = 'BobRay';
+    protected $invalidUsernameTooShort = 'Bo';
+    protected $invalidUsernameTooLong =
+        'asda sdada sdasdasd adasdasda' .
+        'sdasda sdadadadad asdasdad';
+    protected $validEmail = 'bobray@hotmail.com';
+    protected $invalidEmail = 'bobrayhotmail.com';
+    protected $validPhone = '218-143-2345';
+    protected $invalidPhone = '218x123x2345';
+
+    protected function _before() {
+        require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/core/model/user2.class.php';
+    }
+
+    protected function _after() {
+    }
+
+    public function testWorking() {
+        assertTrue(true);
+    }
+
+    /** @throws /Exception */
+    public function testValidateUsername() {
+        $user = Stub::makeEmptyExcept('User', 'validateUsername');
+        $result = $user->validateUsername($this->validUsername);
+        assertTrue($result);
+        $result = $user->validateUsername($this->invalidUsernameTooShort);
+        assertFalse($result);
+        $result = $user->validateUsername($this->invalidUsernameTooLong);
+        assertFalse($result);
+    }
+
+    /** @throws /Exception */
+    public function testValidateEmail() {
+        $user = Stub::makeEmptyExcept('User', 'validateEmail');
+        $result = $user->validateEmail($this->validEmail);
+        assertTrue($result);
+        $result = $user->validateEmail($this->invalidEmail);
+        assertFalse($result);
+    }
+
+    /** @throws /Exception */
+    public function testValidatePhone() {
+        $user = Stub::makeEmptyExcept('User', 'validatePhone');
+        $result = $user->validatePhone($this->validPhone);
+        assertTrue($result);
+        $result = $user->validatePhone($this->invalidPhone);
+        assertFalse($result);
+    }
+
+    public function testAllValid() {
+        $fields = array(
+            'username' => $this->validUsername,
+            'email' => $this->validEmail,
+            'phone' => $this->validPhone,
+        );
+        $user = new User($fields);
+        assertFalse($user->hasErrors());
+        $errors = $user->getErrors();
+        assertEmpty($errors);
+    }
+
+    public function testBadUsernameWithConstructor() {
+        /* Too Short */
+        $fields = array(
+            'username' => $this->invalidUsernameTooShort,
+            'email' => $this->validEmail,
+            'phone' => $this->validPhone,
+        );
+        $user = new User($fields);
+        assertTrue($user->hasErrors());
+        $errors = $user->getErrors();
+        assertNotEmpty($errors);
+        assertEquals(1, count($errors));
+        assertEquals('Invalid username', $errors[0], $errors[0]);
+
+        /* Too long */
+        $fields = array(
+            'username' => $this->invalidUsernameTooLong,
+            'email' => $this->validEmail,
+            'phone' => $this->validPhone,
+        );
+        $user = new User($fields);
+        assertTrue($user->hasErrors());
+        $errors = $user->getErrors();
+        assertNotEmpty($errors);
+        assertEquals(1, count($errors));
+        assertEquals('Invalid username', $errors[0], $errors[0]);
+    }
+
+    public function testBadEmailWithConstructor() {
+        $fields = array(
+            'username' => $this->validUsername,
+            'email' => $this->invalidEmail,
+            'phone' => $this->validPhone,
+        );
+        $user = new User($fields);
+        assertTrue($user->hasErrors());
+        $errors = $user->getErrors();
+        assertNotEmpty($errors);
+        assertEquals(1, count($errors));
+        assertEquals('Invalid email', $errors[0], $errors[0]);
+    }
+
+    public function testBadPhoneWithConstructor() {
+        $fields = array(
+            'username' => $this->validUsername,
+            'email' => $this->validEmail,
+            'phone' => $this->invalidPhone,
+        );
+        $user = new User($fields);
+        assertTrue($user->hasErrors());
+        $errors = $user->getErrors();
+        assertNotEmpty($errors);
+        assertEquals(1, count($errors));
+        assertEquals('Invalid phone', $errors[0], $errors[0]);
+    }
+}
---
+++ core/model/user2.class.php
@@ -0,0 +1,145 @@
+<?php
+
+    class User {
+        protected $errors = array();
+        protected $fields = array(
+            'username' => '',
+            'email' => '',
+            'phone' => ''
+        );
+
+    function __construct($properties = array()){
+        if (!empty($properties)) {
+            foreach ($properties as $key => $value) {
+                $this->set($key, $value);
+            }
+        }
+    }
+
+    /**
+     * Set a User field's value if the value is valid
+     * @param string $fieldName -- name of the field
+     * @param string $value -- value to set
+     * @return bool true on success, false on failure
+     */
+    public function set($fieldName, $value) {
+
+        switch($fieldName) {
+            case 'username':
+                $valid = $this->validateUsername($value);
+                $errorMsg = 'Invalid username';
+                break;
+
+            case 'email':
+                $valid = $this->validateEmail($value);
+                $errorMsg = 'Invalid email';
+                break;
+
+            case 'phone':
+                $valid = $this->validatePhone($value);
+                $errorMsg = 'Invalid phone';
+                break;
+
+            default:
+                $valid = false;
+                $errorMsg = 'Field Not Found';
+
+        }
+
+        if ($valid == false) {
+            $this->addError($errorMsg);
+        } else {
+            $this->fields[$fieldName] = $value;
+        }
+        return $valid;
+    }
+
+    /**
+     * Return the value of a specific field
+     * @param string $fieldName -- field to get
+     * @return mixed|null -- return field value
+     *    or null for unknown field
+     */
+    public function get($fieldName) {
+        if (array_key_exists($fieldName, $this->fields)) {
+            return $this->fields[$fieldName];
+        } else {
+            $this->addError('Unknown Field: ' . $fieldName);
+            return null;
+        }
+    }
+
+    /**
+     * Validate username
+     * @param string $username
+     * @return bool -- true if valid, false if not
+     */
+    public function validateUsername($username) {
+        return (bool)(
+            (strlen($username) <= 50) &&
+            (strlen($username) >= 3)
+        );
+    }
+
+    /**
+     * Validate email
+     * @param string $email
+     * @return bool -- true if valid, false if not
+     */
+    public function validateEmail($email) {
+        return (bool) strpos($email, '@') !== false;
+    }
+
+
+    /**
+     * Validate Phone number
+     *
+     * @param string $phone
+     * @return bool -- true if valid, false if not
+     */
+    public function validatePhone($phone) {
+        $pattern = '/^[0-9\-,.:]+$/';
+        return (bool) preg_match($pattern, $phone);
+    }
+
+    /**
+     * Save User object
+     * @param $user
+     * @return bool
+     */
+    public function save($user) {
+        return true;
+    }
+
+    /**
+     * Add an error to $this->errors
+     * @param string $error
+     */
+    public function addError($error) {
+        $this->errors[] = $error;
+    }
+
+    /**
+     * Get full array of errors
+     * @return array
+     */
+    public function getErrors() {
+        return $this->errors;
+    }
+
+    /**
+     * Clear all errors
+     */
+    public function clearErrors() {
+        $this->errors = array();
+    }
+
+    /**
+     * Report if there are any errors
+     * @return bool -- true if errors, false if not
+     */
+    public function hasErrors() {
+        return !empty($this->errors);
+    }
+
+}

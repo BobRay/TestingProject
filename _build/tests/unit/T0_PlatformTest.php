76bcffc21f01e731b52a6ec0bb5ef54d82c06a23
bobray <bobray@softville.com>
2020-02-20 14:11:08 -0500

    Add first unit test (T0_PlatformTest.php)

---
+++ _build/tests/unit/T0_PlatformTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class T0_PlatformTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /* Tests */
+    public function testWorking()
+    {
+        assertTrue(false);
+    }
+}

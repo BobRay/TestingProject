98be3d9d49f1f621b667164f6829859a308574c6
bobray <bobray@softville.com>
2020-02-20 14:13:23 -0500

    Add second unit test (T1_ and user1.class.php)

---
+++ _build/tests/unit/T1_UserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class T1_UserTest extends \Codeception\Test\Unit {
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before() {
+        require_once 'c:/xampp/htdocs/addons/assets/mycomponents/testingproject/core/model/user1.class.php';
+    }
+
+    protected function _after() {
+    }
+
+    /* Tests */
+    public function testWorking() {
+        assertTrue(true);
+    }
+
+    public function testConstructorWithArguments() {
+        $user = new User1(array(
+            'username' => 'BobRay',
+            'email' => 'bobray@hotmail.com',
+            'phone' => '651-423-1548'
+        ));
+        assertInstanceOf('User1', $user);
+        assertEquals('BobRay', $user->get('username'));
+        assertEquals('bobray@hotmail.com', $user->get('email'));
+        assertEquals('651-423-1548', $user->get('phone'));
+    }
+
+    public function testConstructorNoArguments() {
+        $user = new User1();
+        assertInstanceOf('User1', $user);
+        assertEquals('', $user->get('username'));
+        assertEquals('', $user->get('email'));
+        assertEquals('', $user->get('phone'));
+    }
+}

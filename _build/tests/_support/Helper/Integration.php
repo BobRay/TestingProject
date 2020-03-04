c81b45bf2f75b3388dfab6f967e9eb7653c13ee1
bobray <bobray@softville.com>
2020-03-04 18:22:01 -0500

    Working getModx() helper

--- _build/tests/_support/Helper/Integration.php
+++ _build/tests/_support/Helper/Integration.php
@@ -1,10 +1,19 @@
 <?php
 namespace Helper;
-
-// here you can define custom actions
-// all public methods declared in helper class will be available in $I
+use \modX;
 
 class Integration extends \Codeception\Module
+
 {
+    /**  @var \modX $modx */
+    public $modx;
+
+    public function getModx() {
+        require_once 'c:/xampp/htdocs/test/core/model/modx/modx.class.php';
+        echo "Getting MODX";
+        $this->modx = new modX();
+        $this->modx->initialize('mgr');
+        return $this->modx;
+    }
 
 }
--- _build/tests/_support/Helper/Unit.php
+++ _build/tests/_support/Helper/Unit.php
@@ -7,4 +7,5 @@ namespace Helper;
 class Unit extends \Codeception\Module
 {
 
+
 }

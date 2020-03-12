e38e27a3adc50def85404e5ea29594e3b7423920
bobray <bobray@softville.com>
2020-03-12 00:45:05 -0400

    Update T10_ test

--- _build/tests/integration/T10_UserProcessorTest.php
+++ _build/tests/integration/T10_UserProcessorTest.php
@@ -20,9 +20,9 @@ class T10UserProcessorTest extends \Codeception\Test\Unit
 
     protected function _before()
     {
-        require_once 'c:/xampp/htdocs/test/core/model/modx/modProcessor.class.php';
+        require_once 'C:/xampp/htdocs/test/core/model/modx/modProcessor.class.php';
         /* Get test file from dev. environment */
-        require_once 'c:/xampp/htdocs/addons/core/model/modx/processors/security/user/create.class.php';
+        require_once 'C:/xampp/htdocs/test/core/model/modx/processors/security/user/create.class.php';
         $this->modx = Fixtures::get('modx');
         assertInstanceOf('modX', $this->modx);
 
@@ -42,7 +42,7 @@ class T10UserProcessorTest extends \Codeception\Test\Unit
         assertEquals('modUser', $p->classKey);
     }
 
-    public function testInit()
+    public function testInitialize()
     {
         /** @var modUserCreateProcessor $p */
         $p = $this->processor;
@@ -74,6 +74,14 @@ class T10UserProcessorTest extends \Codeception\Test\Unit
         $p->initialize();
         $result = $p->beforeSave();
         assertTrue($result, print_r($result, true));
+        $object = $p->object;
+        assertInstanceOf('modUser', $object);
+        $username = $object->get('username');
+        assertEquals('Joe99', $username);
+        $profile = $object->getOne('Profile');
+        assertInstanceOf('modUserProfile', $profile);
+        $email = $profile->get('email');
+        assertEquals('joe99@hotmail.com', $email);
     }
 
     public function testAddProfile() {
@@ -82,20 +90,51 @@ class T10UserProcessorTest extends \Codeception\Test\Unit
         $p->initialize();
         $profile = $p->addProfile();
         assertInstanceOf('modUserProfile', $profile);
+        $object = $p->object;
+        assertInstanceOf('modUser', $object);
+        $profile = $object->getOne('Profile');
+        assertInstanceOf('modUserProfile', $profile);
+        $email = $profile->get('email');
+        assertEquals('joe99@hotmail.com', $email);
     }
 
     public function testAfterSave() {
+        $I = $this->tester;
         /** @var modUserCreateProcessor $p */
         $p = $this->processor;
         $p->initialize();
+        $p->process();
         $result = $p->afterSave();
         assertTrue($result);
+        $userTable = $this->_getTableName('modUser');
+        $profileTable = $this->_getTableName('modUserProfile');
+        $I->seeInDatabase($userTable, array('username' => 'Joe99'));
+        $I->seeInDatabase($profileTable, array('email' => 'joe99@hotmail.com'));
     }
 
+    /** Test full processor action with runProcessor() */
     public function testFull() {
+        $I = $this->tester;
         $fields = $this->fields;
-//       $result = $this->modx->runProcessor('security/user/create', $fields);
-//         assertInstanceOf('modProcessorResponse', $result);
+        /* Bypass MODX bug */
+        $options = array('processors_path' => 'C:/xampp/htdocs/test/core/model/modx/processors/security/');
+        $result = $this->modx->runProcessor('user/create', $fields, $options);
+        assertInstanceOf('modProcessorResponse', $result);
+        $userTable = $this->_getTableName('modUser');
+        $profileTable = $this->_getTableName('modUserProfile');
+        $I->seeInDatabase($userTable, array('username' => 'Joe99'));
+        $I->seeInDatabase($profileTable, array('email' => 'joe99@hotmail.com'));
+    }
 
+
+    /**
+     * Utility function to remove back-ticks from table name
+     *
+     * @param $class
+     * @return string
+     */
+    public function _getTableName($class) {
+        $tableName = $this->modx->getTableName($class);
+        return str_replace('`', '', $tableName);
     }
 }

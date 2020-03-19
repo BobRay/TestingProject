d62f2b9f88193f0a3a195b2567cf0ba3aee8c320
bobray <bobray@softville.com>
2020-03-18 23:37:44 -0400

    Add lexicon stuff to T13_LoginCest.php

--- _build/tests/acceptance/T13_LoginCest.php
+++ _build/tests/acceptance/T13_LoginCest.php
@@ -1,36 +1,50 @@
-<?php 
+<?php
+use Codeception\Util\Fixtures;
 
 class T13_LoginCest
 {
+    /** @var modX $modx */
+    protected $modx;
     public function _before(AcceptanceTester $I)
     {
+        $this->modx = Fixtures::get('modx');
+        $this->modx->lexicon->load('en:login');
     }
 
     // tests
     /** @throws Exception */
-    public function tryToTest(AcceptanceTester $I)
+    public function testLogin(AcceptanceTester $I)
     {
+        $errorMsg = $this->modx->lexicon('login_cannot_locate_account');
+        $delay = 1;
         /* Incorrect username */
         $I->amOnPage('manager');
         $I->fillField('#modx-login-username', 'XJoeTester');
         $I->fillField('#modx-login-password', 'TesterPassword');
+        $I->wait($delay);
         $I->click('#modx-login-btn');
-        $I->see('The username or password you entered is incorrect. Please check the username, re-type the password, and try again.');
+        $I->see($errorMsg);
+        $I->wait($delay);
 
         /* Incorrect password */
         $I->amOnPage('manager');
         $I->fillField('#modx-login-username', 'JoeTester');
         $I->fillField('#modx-login-password', 'XTesterPassword');
+        $I->wait($delay);
         $I->click('#modx-login-btn');
-        $I->see('The username or password you entered is incorrect. Please check the username, re-type the password, and try again.');
+        $I->see($errorMsg);
+        $I->wait($delay);
 
         /* Correct credentials */
         $I->fillField('#modx-login-username', 'JoeTester');
         $I->fillField('#modx-login-password', 'TesterPassword');
+        $I->wait($delay);
         $I->click('#modx-login-btn');
         $I->see('Content');
         $I->see('Manage');
         $I->dontSee('#modx-login-username');
         $I->dontSee('#modx-login-password');
+        $I->dontSee($errorMsg);
+        $I->wait($delay);
     }
 }

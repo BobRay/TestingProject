180617bcf527b59b837c9446b78a36823e2a432f
bobray <bobray@softville.com>
2020-03-23 14:26:14 -0400

    Add T14_LoginPageCest.php

---
+++ _build/tests/acceptance/T14_LoginPageCest.php
@@ -0,0 +1,24 @@
+<?php
+use Page\Acceptance\Login as LoginPage;
+
+class T14_LoginPageCest
+{
+
+    public function _before(\AcceptanceTester $I)
+    {
+
+    }
+
+    // tests
+    /** @throws \Exception */
+    public function testLoginLogout(AcceptanceTester $I)
+    {
+        $I->wantTo('Log In');
+        $loginPage = new LoginPage($I);
+        $loginPage->login();
+        $I->see('Content');
+        $I->see('Manage');
+        $loginPage->logout();
+        $I->wait(2);
+    }
+}

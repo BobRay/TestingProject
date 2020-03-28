08d605067a9c0b55295a21b69b0ca3847ef7bec4
bobray <bobray@softville.com>
2020-03-28 16:36:41 -0400

    Code cleanup for UserPage

--- _build/tests/_support/Page/Acceptance/UserPage.php
+++ _build/tests/_support/Page/Acceptance/UserPage.php
@@ -3,6 +3,10 @@ namespace Page\Acceptance;
 
 class UserPage
 {
+    /**
+     * @var \AcceptanceTester;
+     */
+    protected $acceptanceTester;
 
     public static $newUserButton = "//button[contains(text(), 'New User')]";
     public static $userSaveButton = "//button[contains(text(), 'Save')]";
@@ -63,25 +67,8 @@ class UserPage
     );
 
 
-
-    /**
-     * Basic route example for your current URL
-     * You can append any additional parameter to URL
-     * and use it in tests like: Page\Edit::route('/123-post');
-     */
-    public static function route($param)
-    {
-        return static::$URL.$param;
-    }
-
-    /**
-     * @var \AcceptanceTester;
-     */
-    protected $acceptanceTester;
-
     public function __construct(\AcceptanceTester $I)
     {
         $this->acceptanceTester = $I;
     }
-
 }

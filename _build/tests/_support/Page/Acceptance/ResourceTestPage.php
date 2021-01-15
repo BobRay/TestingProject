f1eca5c41a63b87ffe8927e029764bedb49de709
bobray <bobray@softville.com>
2021-01-15 17:36:07 -0500

    More code cleanup

--- _build/tests/_support/Page/Acceptance/ResourceTestPage.php
+++ _build/tests/_support/Page/Acceptance/ResourceTestPage.php
@@ -27,27 +27,43 @@ class ResourceTestPage
         "//span[starts-with(@class,'x-tab-strip-text')
          and text()='Permissions']";
 
-    public static $resourceGroupAccessTab = "//span[contains(text(),'Resource Group Access')]";
+    public static $resourceGroupAccessTab =
+        "//span[contains(text(),'Resource Group Access')]";
 
-    public static $addResourceGroupButton = "//button[contains(text(),'Add Resource Group')]";
+    public static $addResourceGroupButton =
+        "//button[contains(text(),'Add Resource Group')]";
 
-    public static $resourceGroupInput = "//input[starts-with(@id,'modx-crgact') and contains(@id, 'resource-group')]";
+    public static $resourceGroupInput =
+        "//input[starts-with(@id,'modx-crgact') 
+        and contains(@id, 'resource-group')]";
 
-    public static $privateResourcesOption = "//div[text()='PrivateResources']";
+    public static $privateResourcesOption =
+        "//div[text()='PrivateResources']";
 
-    public static $contextInput = "//input[starts-with(@id,'modx-crgact') and contains(@id, '-context')]";
+    public static $contextInput =
+        "//input[starts-with(@id,'modx-crgact') 
+        and contains(@id, '-context')]";
 
-    public static $mgrOption = "//span[text()='(mgr)']";
+    public static $mgrOption =
+        "//span[text()='(mgr)']";
 
-    public static $authorityInput = "//input[starts-with(@id,'modx-crgact') and contains(@id, 'authority')]";
+    public static $authorityInput =
+        "//input[starts-with(@id,'modx-crgact') 
+        and contains(@id, 'authority')]";
 
-    public static $testUserOption = "//div[text()='TestUser - 15' and contains(@class,'x-combo-list-item')]";
+    public static $testUserOption =
+        "//div[text()='TestUser - 15' 
+        and contains(@class,'x-combo-list-item')]";
 
-    public static $policyInput = "//*[starts-with(@id,'x-form-el-modx-crg') and contains(@id,'policy')]";
+    public static $policyInput =
+        "//*[starts-with(@id,'x-form-el-modx-crg') 
+        and contains(@id,'policy')]";
 
-    public static $resourceOption = "//div[text()='Resource']";
+    public static $resourceOption =
+        "//div[text()='Resource']";
 
-    public static $addResourcePanelSaveButton = "//span[starts-with(@id,'ext-comp')]/em/button[text()='Save']";
+    public static $addResourcePanelSaveButton =
+        "//span[starts-with(@id,'ext-comp')]/em/button[text()='Save']";
 
     /**
      * Basic route example for your current URL

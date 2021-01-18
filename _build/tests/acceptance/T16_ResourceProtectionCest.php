08fe4132a700798e50fb33c9bb7701d9f5278aaa
bobray <bobray@softville.com>
2021-01-18 12:31:10 -0500

    Code formatting

--- _build/tests/acceptance/T16_ResourceProtectionCest.php
+++ _build/tests/acceptance/T16_ResourceProtectionCest.php
@@ -10,7 +10,8 @@ class T16_ResourceProtectionCest
     public $modx;
     private const ROLES = array('TestUser');
     private const USER_GROUPS = array('PublicUsers', 'PrivateUsers');
-    private const RESOURCE_GROUPS = array('PublicResources', 'PrivateResources');
+    private const RESOURCE_GROUPS = array('PublicResources',
+        'PrivateResources');
 
     public static function _before(\Step\Acceptance\Objects $I) {
         $users = include codecept_data_dir() .
@@ -20,14 +21,16 @@ class T16_ResourceProtectionCest
         $modx = Fixtures::get('modx');
         assertTrue($modx instanceof modX);
         $I->createRoles($modx, self::ROLES);
-        $I->createUserGroups($modx, self::USER_GROUPS);
+        $I->createUserGroups($modx,
+            self::USER_GROUPS);
         $I->createUsers($modx, $users);
-        $I->createResourceGroups($modx, self::RESOURCE_GROUPS);
+        $I->createResourceGroups($modx,
+            self::RESOURCE_GROUPS);
         $I->createResources($modx, $resources);
     }
     
     public static function _after(\Step\Acceptance\Objects $I) {
-        // return;  /* allows examination of objects and ACL */
+   // return;  /* allows examination of objects and ACL */
         $users = include codecept_data_dir() .
             '/user_data.php';
         $resources = include codecept_data_dir() .
@@ -35,9 +38,11 @@ class T16_ResourceProtectionCest
         $modx = Fixtures::get('modx');
         assertTrue($modx instanceof modX);
         $I->removeRoles($modx, self::ROLES);
-        $I->removeUserGroups($modx, self::USER_GROUPS);
+        $I->removeUserGroups($modx,
+            self::USER_GROUPS);
         $I->removeUsers($modx, $users);
-        $I->removeResourceGroups($modx, self::RESOURCE_GROUPS);
+        $I->removeResourceGroups($modx,
+            self::RESOURCE_GROUPS);
         $I->removeResources($modx, $resources);
     }
     // test

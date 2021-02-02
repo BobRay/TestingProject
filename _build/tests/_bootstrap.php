899c448b46db97f50eb7153ffb2807bdabd08494
bobray <bobray@softville.com>
2021-02-02 17:38:33 -0500

    Reduce master _bootstrap.php to contain only vendor includes

--- _build/tests/_bootstrap.php
+++ _build/tests/_bootstrap.php
@@ -1,13 +1,4 @@
 <?php
-use Codeception\Util\Fixtures;
-
 require_once 'c:/xampp/htdocs/addons/vendor/autoload.php';
 require_once 'c:/xampp/htdocs/addons/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
 
-/*require_once 'c:/xampp/htdocs/test/core/model/modx/modx.class.php';
-echo "Getting MODX";
-$modx = new modX();
-$modx->getRequest();
-$modx->getService('error', 'error.modError', '', '');
-$modx->initialize('mgr');
-Fixtures::add('modx', $modx);*/
